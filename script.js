40eff1ea45721e726b28cae137f60cb6ec308dc1
Admin <conore21@gmail.comgit config --gloval user.name RaoulTheF00l>
2026-07-04 12:09:28 -0400

    added base functions

--- script.js
+++ script.js
@@ -33,3 +33,50 @@ const breakthroughButton = document.getElementById("breakthroughButton");
 // BUTTON EVENT LISTENERS
 // These connect button clicks to game functions.
 // =====================================================
+
+cultivationButton.addEventListener("click", cultivate);
+meditateButton.addEventListener("click", meditate);
+workButton.addEventListener("click", work);
+exploreButton.addEventListener("click", explore);
+breakthroughButton.addEventListener("click", attemptBreakthrough);
+
+// =====================================================
+// PLAYER ACTIONS
+// Each action changes the player state, add a log message,
+// then updates the screen
+// =====================================================
+
+function cultivate(){
+    player.qi += 8;
+    player.innerDemon += 2;
+
+    addLog("You sit in the meditation chamber and circulate qi through your merdians.");
+
+    updateGame();
+}
+
+function meditate() {
+    player.spirit += 2;
+    player.body += 1;
+
+    addLog("You spend the day hauling water in the outer sect. It's not glorious, but it pays.");
+
+    updateGame();
+}
+
+function explore() {
+    const roll = Math.random
+
+    if (roll < 0.4) {
+        addLog("You find a small pouch of spirit stones hidden near a ruined shrine.");
+    } else if (roll < 0.7) {
+        player.qi += 10;
+        player.innerDemon += 4;
+        addLog("You discovered a strange herb. It strenghthened your qi, butt leaves your thoughts restless.")
+    } else {
+        player.body -= 1;
+        addLog("A wild beast wounds you before you manage to escape");
+    }
+
+    updateGame();
+}
